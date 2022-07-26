37ab4c89a0531161a9202f255bfd603a37a9e374
TalhaSMinhas <talha.salman.minhas@icloud.com>
2022-07-26 10:35:51 +0200

    Initial Commit

---
+++ swiftUIPlayground/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  swiftUIPlayground
+//
+//  Created by Talha Minhas on 2022/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //@State private var name = "Poop"
+    @State private var value1 = ""
+    @State private var value2 = ""
+    
+    private var sum: Int{
+        get{
+            (Int(value1) ?? 0) + (Int(value2) ?? 0)
+        }
+    }
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                TextField("Type First Value Here", text: $value1).keyboardType(.numberPad).padding(.all)
+                    .foregroundColor(Color.pink)
+                
+                TextField("Type Second Value Here", text: $value2)
+                    .keyboardType(.numberPad).padding(.all)
+                    .foregroundColor(Color.cyan)
+                        
+                Text("Sum: \(sum)").padding(.all).foregroundColor(Color.green)
+                
+            }.navigationTitle("Two Number Adder")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ swiftUIPlayground/swiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPlaygroundApp.swift
+//  swiftUIPlayground
+//
+//  Created by Talha Minhas on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
